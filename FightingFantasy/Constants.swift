346c9e29948140f002f53d7470a6839d8adba000
Tony Smith <tony@electricimp.com>
2017-11-02 21:40:50 +0000

    Separate constants into dedicated file

---
+++ FightingFantasy/Constants.swift
@@ -0,0 +1,43 @@
+
+//  FightingFantasy
+//  Created by Tony Smith on 02/11/2017.
+//  Copyright © 2017 Tony Smith. All rights reserved.
+
+
+import Foundation
+
+
+// MARK: Constants
+
+// Game types
+
+let kGameNone = -1
+let kGameWarlock = 0
+let kGameCitadel = 1
+let kGameForestDoom = 2
+let kGameDeathtrap = 3
+let kGameCityThieves = 4
+let kGameHouseHell = 5
+let kGameCavernsSnow = 6
+let kGameIslandLizard = 7
+let kGameTempleTerror = 8
+let kGameTrialChampions = 9
+let kGameCreatureHavoc = 10
+let kGameReturnFiretop = 11
+let kGameEyeDragon = 12
+let kGamePortPeril = 13
+let kGameSorceryWizard = 20
+let kGameSorceryFighter = 21
+
+// Colours for text
+
+let kTextColourBlack = 0
+let kTextColourRed = 1
+let kTextColourGrey = 2
+
+// Potions
+
+let kPotionNone = -1
+let kPotionDexterity = 0
+let kPotionStrength = 1
+let kPotionFortune = 2
