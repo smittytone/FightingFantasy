7abff5cbc91f8e77ce468ded6639269075da23ce
Tony Smith <tony@electricimp.com>
2017-11-18 20:19:14 +0000

    Create custom NSTableCellView by adding an FFIconButton property

---
+++ FightingFantasy/FFTableCellView.swift
@@ -0,0 +1,13 @@
+
+//  FightingFantasy
+//  Created by Tony Smith on 10/30/17.
+//  Copyright © 2017 Tony Smith. All rights reserved.
+
+
+import Cocoa
+
+class FFTableCellView: NSTableCellView {
+
+    @IBOutlet weak var button: FFIconButton!
+
+}
