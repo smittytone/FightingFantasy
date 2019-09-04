b6da0cc06939ca45fa5d3717444aa3e063df541c
smittytone <smittytone@icloud.com>
2019-09-04 10:03:07 +0100

    Update copyright dates

--- FightingFantasy/Constants.swift
+++ FightingFantasy/Constants.swift
@@ -1,9 +1,9 @@
 
 //  FightingFantasy
 //  Created by Tony Smith on 02/11/2017.
-//  Software © 2017-18 Tony Smith. All rights reserved.
+//  Software © 2017-19 Tony Smith. All rights reserved.
 //  Software ONLY issued under MIT Licence
-//  Fighting Fantasy © 2016 Steve Jackson and Ian Livingstone
+//  Fighting Fantasy © 2016-19 Steve Jackson and Ian Livingstone
 
 
 import Foundation
