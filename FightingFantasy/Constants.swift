ce7fcb6301334fc65d2bafc2d30f853fdf3afa41
smittytone <smittytone@icloud.com>
2023-10-18 18:00:24 +0100

    Latest upload @ Wed 18 Oct 2023 18:00:24 BST

--- FightingFantasy/Constants.swift
+++ FightingFantasy/Constants.swift
@@ -9,9 +9,36 @@
 import Foundation
 
 
-// MARK: Constants
+// MARK: Enumerations
 
-// Game types
+enum FFGameType: Int {
+    case kGameNone              = -1
+    case kGameWarlock           = 0
+    case kGameCitadel           = 1
+    case kGameForestDoom        = 2
+    case kGameDeathtrap         = 3
+    case kGameCityThieves       = 4
+    case kGameHouseHell         = 5
+    case kGameCavernsSnow       = 6
+    case kGameIslandLizard      = 7
+    case kGameTempleTerror      = 8
+    case kGameTrialChampions    = 9
+    case kGameCreatureHavoc     = 10
+    case kGameReturnFiretop     = 11
+    case kGameEyeDragon         = 12
+    case kGamePortPeril         = 13
+    case kGameSorceryWizard     = 20
+    case kGameSorceryFighter    = 21
+}
+
+enum FFPotionType: Int {
+    case kPotionNone            = -1
+    case kPotionDexterity       = 0
+    case kPotionStrength        = 1
+    case kPotionFortune         = 2
+}
+
+// Game Types
 
 let kGameNone = -1
 let kGameWarlock = 0
@@ -30,12 +57,6 @@ let kGameEyeDragon = 12
 let kGamePortPeril = 13
 let kGameSorceryWizard = 20
 let kGameSorceryFighter = 21
-
-// Colours for text
-
-let kTextColourBlack = 0
-let kTextColourRed = 1
-let kTextColourGrey = 2
 
 // Potions
 
